d30f72c5b76e4becb8ad749e24d61b45b01bfa99
Maksymilian Mozolewski <makspl17@gmail.com>
2024-01-15 12:21:45 +0000

    Update worker-script.js

--- worker-script.js
+++ worker-script.js
@@ -123,8 +123,7 @@ function calculate_sentiment(words) {
   if (words.length > 0) {
     console.log("Calculating sentiment for: " + words)
     let sentiment_carriers = words.map(w => AFINN[w]).filter(Boolean)
-    console.log("sentiment carriers: " + sentiment_carriers)
-    return sentiment_carriers.reduce(Math.sum) / sentiment_carriers.length
+    return sentiment_carriers.reduce((a, b) => a + b, 0) / sentiment_carriers.length
   } else {
     return 0
   }
