cf0c8077f561df9c01c35e7732f065e7e6c58342
makspll <makspl17@gmail.com>
2024-07-28 15:20:52 +0100

    add more safety

--- src/main.js
+++ src/main.js
@@ -419,7 +419,7 @@ async function get_kv_object(key, cache_seconds, defaultVal = "{}") {
 }
 
 async function store_kv_object(key, value) {
-    if (value != null) {
+    if (value != null && value != undefined) {
         console.log("storing: " + value + " at key: " + key)
         let data = JSON.stringify(value);
         await ENV.KV_STORE.put(key, data)
