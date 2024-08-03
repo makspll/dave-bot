43b48e6c356844c9e07ccc4d60ef9a0d8893e53e
Maksymilian Mozolewski <makspl17@gmail.com>
2024-08-03 08:49:17 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -40,6 +40,9 @@ const COMMANDS = {
         return sendMessage("You have been opted out and your dave record wiped out, to opt back in use '/optin' the bot might take an hour or so to stop replying.", payload.message.chat.id, 0, null)
     },
     "optin": async (payload, args) => {
+        console.log(payload.message)
+        console.log(payload.message.chat)
+        console.log(payload.message.chat.id)
         if (payload.message.chat.id != ENV.MAIN_CHAT_ID) {
             console.log("can't opt int from:", payload.message, payload.message.chat, payload.messahe.chat.id)
             return
