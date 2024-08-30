7565609f5aeea419bcde954fa4110461d14c698c
makspll <makspl17@gmail.com>
2024-08-30 16:07:52 +0100

    integers

--- dave_bot/migrations/0004_add_scraping_tables.sql
+++ dave_bot/migrations/0004_add_scraping_tables.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE property_snapshots (
     property_id VARCHAR(255) NOT NULL,
-    search_id BIGINT NOT NULL,
+    search_id INTEGER NOT NULL,
     url VARCHAR(255) NOT NULL,
     address VARCHAR(255) NOT NULL,
     price_per_month INTEGER NOT NULL,
@@ -18,15 +18,15 @@ CREATE TABLE property_snapshots (
 );
 
 CREATE TABLE searches (
-    search_id BIGINT PRIMARY KEY NOT NULL AUTOINCREMENT,
-    user_query_id BIGINT NOT NULL,
+    search_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_query_id INTEGER NOT NULL,
     search_datetime TIMESTAMP NOT NULL DEFAULT (TIMESTAMP('now')),
 
     FOREIGN KEY (user_query_id) REFERENCES user_queries(user_query_id) ON DELETE CASCADE
 );
 
 CREATE TABLE user_queries (
-    user_query_id BIGINT PRIMARY KEY NOT NULL AUTOINCREMENT,
+    user_query_id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id BIGINT NOT NULL,
     query VARCHAR(255) NOT NULL UNIQUE,
     creation_date DATE NOT NULL DEFAULT (DATE('now')),
--- dave_bot/migrations/0005_add_permissions_table.sql
+++ dave_bot/migrations/0005_add_permissions_table.sql
@@ -1,7 +1,7 @@
 -- Migration number: 0005 	 2024-08-29T21:21:32.516Z
 
 CREATE TABLE permissions (
-    permission_id INT PRIMARY KEY NOT NULL AUTOINCREMENT,
+    permission_id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id BIGINT NOT NULL,
     permission VARCHAR(255) NOT NULL,
     creation_date DATE NOT NULL DEFAULT (DATE('now')),
