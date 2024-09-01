d28713328427651d95e7e8e30e44af9c6c2cb5fc
makspll <makspl17@gmail.com>
2024-09-01 22:32:00 +0100

    fix error

--- dave_bot/src/commands.ts
+++ dave_bot/src/commands.ts
@@ -46,7 +46,7 @@ export class Command<T extends any[]> {
             const parsed_args = this.args.get_values(args);
             console.log("Parsed args: ", parsed_args)
             await this.callback(payload, settings, parsed_args);
-        } catch (e) {
+        } catch (e: any) {
             if (e instanceof UserErrorException) {
                 await sendMessage({
                     api_key: settings.telegram_api_key,
@@ -59,7 +59,7 @@ export class Command<T extends any[]> {
                     delay: 0
                 })
             } else {
-                console.error("Error running command: ", e)
+                console.error("Error running command: ", e, e.toString())
                 sendCommandMessage(payload, settings, "There was an error processing your command, please try again later")
                 throw e
             }
