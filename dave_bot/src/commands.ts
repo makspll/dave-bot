3b3fd16c682958102d3f2f82392acd550f5ad893
makspll <makspl17@gmail.com>
2024-09-01 15:20:19 +0100

    fix no log

--- dave_bot/src/commands.ts
+++ dave_bot/src/commands.ts
@@ -418,6 +418,7 @@ export async function initiate_property_search(payload: TelegramMessage, setting
     try {
         await scrape_zoopla(query, settings)
     } catch (e) {
+        console.error("error scraping", e)
         await sendCommandMessage(payload, settings, "There was an error searching for properties, please try again later")
     }
 }
