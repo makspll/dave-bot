4c10c7221e26a2f4d404ad717771254f8b86df35
makspll <makspl17@gmail.com>
2026-06-18 19:54:06 +0100

    asd

--- dave_bot/src/poople.ts
+++ dave_bot/src/poople.ts
@@ -29,12 +29,12 @@ export function score_from_poople_shareable(shareable: string): PoopleScore {
     const split = shareable.split(' ');
     const number = parseInt(split[1].replace('#', ''));
     const scores = split[2].split('/');
-    const guesses = Math.min(parseFloat(scores[0]), 1);
-    // const best = Math.min(parseFloat(scores[1]), 1);
+    const guesses = Math.max(parseFloat(scores[0]), 1);
+    const best = Math.max(parseFloat(scores[1]), 1);
 
     return {
         'poopleNo': number,
-        'score': guesses,
+        'score': guesses - best,
     };
 }
 
--- dave_bot/src/types/poople.ts
+++ dave_bot/src/types/poople.ts
@@ -1,5 +1,5 @@
 
-export const FIRST_POOPLE_DATE = new Date('2025-08-16')
+export const FIRST_POOPLE_DATE = new Date('2025-08-15')
 
 export interface PoopleScore {
     poopleNo: number,
